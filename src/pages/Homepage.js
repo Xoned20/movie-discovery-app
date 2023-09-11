07daf29759531ceec3593ec76c6d2a312747d761
Jephery Cyril <cyriljeph@gmail.com>
2023-09-11 03:53:22 +0100

    add

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,72 +1,121 @@
-import React from 'react'
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import MovieCard from "../components/MovieCard";
-import Logo from "../images/Logo.png"
-import Menu from "../images/Menu.png"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import MovieCard from '../components/MovieCard';
+import Logo from '../images/Logo.png';
+import Menu from '../images/Menu.png';
+import Rating from '../images/Rating.png';
+import watchTrailer from '../images/watchTrailer.png';
 import '../App.css';
 
-
 const Homepage = () => {
+  const API_URL = 'https://api.themoviedb.org/3';
+  const BACKGROUND_IMAGE_PATH = 'https://image.tmdb.org/t/p/w1280/';
+  const API_KEY = '42486eb24e530fd06d7241d3330e3684'; // Replace with your actual TMDB API key
+  const ITEMS_PER_PAGE = 12; // Number of movies per page
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [searchKey, setSearchKey] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const API_URL = "https://api.themoviedb.org/3"
-    const BACKGROUND_IMAGE_PATH = "https://image.tmdb.org/t/p/w1280/"
-    const API_KEY = "42486eb24e530fd06d7241d3330e3684"
-    const [movies, setMovies] = useState([])
-    const [selectedMovie, setSelectedMovie] = useState({})
-    const [searchKey, setSearchKey] = useState("")
-
-    const fetchMovies = async (searchKey) => {
-    const type = searchKey ? "search" : "discover"
-    const {data:{results}} = await axios.get(`${API_URL}/${type}/movie`,{
+  const fetchBestMovies = async () => {
+    const { data } = await axios.get(`${API_URL}/discover/movie`, {
       params: {
         api_key: API_KEY,
-        query: searchKey
-      }
-    })
-    setMovies(results)
+        sort_by: 'popularity.desc',
+      },
+    });
+    setMovies(data.results.slice(0, 12)); // Get the best 5 movies
+  };
+
+  const fetchMoviesBySearch = async (searchKey) => {
+    const { data } = await axios.get(`${API_URL}/search/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    if (searchKey) {
+      fetchMoviesBySearch(searchKey);
+    } else {
+      fetchBestMovies();
+    }
+  }, [searchKey]);
+
+  const renderMovies = () =>
+    movies.map((movie) => (
+      <MovieCard key={movie.id} movie={movie} selectMovie={setSelectedMovie} />
+    ));
+
+  const handlePaginationClick = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const totalPages = Math.ceil(movies.length / ITEMS_PER_PAGE);
+  const paginationButtons = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    paginationButtons.push(
+      <button
+        key={i}
+        onClick={() => handlePaginationClick(i)}
+        className={currentPage === i ? 'active' : ''}
+      >
+        {i}
+      </button>
+    );
   }
 
-  useEffect(() =>{
-    fetchMovies(searchKey)
-  })
-
-
-  const renderMovies = () => (
-    movies.map(movie => (
-      <MovieCard
-      key={movie.id}
-      movie={movie}
-      selectMovie={setSelectedMovie}
-      />
-    ))
-  )
- 
-  const searchMovies = (e) =>{
-    e.preventDefault()
-    fetchMovies()
-  }
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setCurrentPage(1); // Reset to the first page when searching
+    fetchMoviesBySearch(searchKey);
+  };
 
   return (
     <div className="App">
-      <div className={"info"}  style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)) ,url(${BACKGROUND_IMAGE_PATH}${selectedMovie.backdrop_path})`}}>
+      <div
+        className={'info'}
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)) ,url(${BACKGROUND_IMAGE_PATH}${selectedMovie.backdrop_path})`,
+        }}
+      >
         <header>
-          <p className={"title"}><img src={Logo} alt='moviebox'/></p>
-          <form onSubmit={searchMovies}>
-            <input type={"text"} onChange={(e) => setSearchKey(e.target.value)} />
+          <p className={'title'}>
+            <img src={Logo} alt="moviebox" />
+          </p>
+          <form onSubmit={handleSearchSubmit}>
+            <input
+              type={'text'}
+              placeholder={'What do you want to watch?'}
+              value={searchKey}
+              onChange={(e) => setSearchKey(e.target.value)}
+            />
+            <button type="search">Search</button>
           </form>
-          <p className={"title"}><img src={Menu} alt='signup'/></p>
+          <p className={'title'}>
+            <img src={Menu} alt="signup" />
+          </p>
         </header>
-        <div className={"info-content"}>
-          <h1>{selectedMovie.title}</h1>
-          {selectedMovie.overview ? <p>{selectedMovie.overview}</p> : null}
+        <div className={'info-content'}>
+            <h1>{selectedMovie.title}</h1>
+            <img src={Rating} alt="Rating" />
+            <p>{selectedMovie.overview}</p>
+            <img src={watchTrailer} alt='watch trailer' />
         </div>
       </div>
       <div className="container">
-        {renderMovies()}
+        {renderMovies().slice(
+          (currentPage - 1) * ITEMS_PER_PAGE,
+          currentPage * ITEMS_PER_PAGE
+        )}
       </div>
+      <div className="pagination">{paginationButtons}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Homepage;
